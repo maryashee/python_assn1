5f8cc4a1bf3b38d46a8d478d550308e9f86adaed
maryashee <shikomaryk@gmail.com>
2025-08-03 18:31:46 +0300

    first commit

---
+++ calc.py
@@ -0,0 +1,23 @@
+num1=float(input("enter the first number"))
+num2=float(input("enter the second number"))
+operation =input("Enter the operation (+, -, *, /): ")
+
+if operation=="+" :
+    result= num1 + num2
+    print(f"{num1} + {num2}= {result}")
+
+elif operation=="-" :
+    result= num1-num2
+    print(f"{num1} - {num2} = {result}")
+
+elif operation=="*" :
+    result= num1*num2
+    print(f"{num1} *{num2}= {result}")
+
+elif operation=="/" :
+      if num2 !=0 :
+        result=num1/num2
+        print(f"{num1} /{num2}= {result}")
+        
+      else :print("error!")
+
