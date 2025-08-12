62f1482dc71ff7519c4018c7a903e3b5c9e00a1e
maryashee <shikomaryk@gmail.com>
2025-08-12 07:09:04 +0300

    this is my python week 2 assignment

---
+++ assn2.py
@@ -0,0 +1,18 @@
+my_list =[]
+my_list.extend({10, 20, 30, 40})
+print(my_list)
+
+my_list.insert(2, 15)
+print(my_list)
+
+my_list.extend([50, 60, 70])
+print(my_list)
+
+my_list.remove(70)
+print(my_list)
+
+my_list.sort()
+print(my_list)
+
+print(my_list[3])
+
